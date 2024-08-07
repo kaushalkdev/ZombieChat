dd5566fd4648b47e06996d5c7004d8501010af73
Kaushal <107100647+kaushalkaajtak@users.noreply.github.com>
2024-08-07 15:35:52 +0530

    Commit msg - Fixed migration related issues.

--- app/src/main/java/com/example/zombiechat/AllUsersRecyclerAdapter.java
+++ app/src/main/java/com/example/zombiechat/AllUsersRecyclerAdapter.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -20,18 +19,16 @@ public class AllUsersRecyclerAdapter extends RecyclerView.Adapter<ViewHolder> {
 
 
     private static final String TAG = "AllUsersRecyclerAdapter";
-    List<SingleUserModel> userModels  ;
-    private String uid;
+    List<SingleUserModel> userModels;
 
-    public AllUsersRecyclerAdapter(List<SingleUserModel> userModels , String uid) {
+    public AllUsersRecyclerAdapter(List<SingleUserModel> userModels, String uid) {
         this.userModels = userModels;
-        this.uid = uid;
     }
+
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-
         Log.d(TAG, "onCreateViewHolder: ");
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.single_all_user_layout, viewGroup, false);
         return new ViewHolder(view);
@@ -40,21 +37,17 @@ public class AllUsersRecyclerAdapter extends RecyclerView.Adapter<ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i) {
 
-
-             viewHolder.username.setText(userModels.get(i).getName());
-             viewHolder.userstatus.setText(userModels.get(i).getStatus());
-             viewHolder.setImage(userModels.get(i).getImage());
-             viewHolder.setOnclick(userModels.get(i).getUserid(),userModels.get(i).getName());
-
-
-
+        viewHolder.username.setText(userModels.get(i).getName());
+        viewHolder.userstatus.setText(userModels.get(i).getStatus());
+        viewHolder.setImage(userModels.get(i).getImage());
+        viewHolder.setOnclick(userModels.get(i).getUserid(), userModels.get(i).getName());
 
 
     }
 
     @Override
     public int getItemCount() {
-        Log.d(TAG, "getItemCount: "+ userModels.size());
+        Log.d(TAG, "getItemCount: " + userModels.size());
         return userModels.size();
     }
 }
@@ -63,39 +56,33 @@ public class AllUsersRecyclerAdapter extends RecyclerView.Adapter<ViewHolder> {
 class ViewHolder extends RecyclerView.ViewHolder {
 
 
-    TextView username,userstatus;
+    TextView username, userstatus;
     CircleImageView userimage;
+
     public ViewHolder(@NonNull View itemView) {
 
 
         super(itemView);
 
-         username = itemView.findViewById(R.id.user_name);
-         userstatus = itemView.findViewById(R.id.user_status);
-         userimage = itemView.findViewById(R.id.user_image);
+        username = itemView.findViewById(R.id.user_name);
+        userstatus = itemView.findViewById(R.id.user_status);
+        userimage = itemView.findViewById(R.id.user_image);
 
 
     }
 
     public void setImage(String image) {
 
-        Picasso.with(itemView.getContext())
-                .load(image)
-                .error(R.drawable.default_user)
-                .placeholder(R.drawable.default_user)
-                .into(userimage);
+        Picasso.with(itemView.getContext()).load(image).error(R.drawable.default_user).placeholder(R.drawable.default_user).into(userimage);
     }
 
     public void setOnclick(final String userid, final String name) {
 
-        itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent userProfileIntent = new Intent(itemView.getContext(),UserProfile.class);
-                userProfileIntent.putExtra("uid",userid);
-                userProfileIntent.putExtra("name",name);
-                itemView.getContext().startActivity(userProfileIntent);
-            }
+        itemView.setOnClickListener(c -> {
+            Intent userProfileIntent = new Intent(itemView.getContext(), UserProfile.class);
+            userProfileIntent.putExtra("uid", userid);
+            userProfileIntent.putExtra("name", name);
+            itemView.getContext().startActivity(userProfileIntent);
         });
 
     }
